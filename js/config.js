b42ccbcc439a94e20d9fa03d959d2df742467e18
P.N.S Sri vardhan <211801410005@cutmap.ac.in>
2025-04-27 00:22:15 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 // config.js
-const API_BASE_URL = 'http://localhost:5000/api'; // Declare once
+const API_BASE_URL = 'https://smo-backend-production.up.railway.app/api'; // Declare once
 
 // Export the API URL to use in other files
 export { API_BASE_URL };
+
